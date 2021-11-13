043b22304d63b7db23d68f8e4621350c23a17235
arafathsiam99 <md.arafathislamsiamnstuam14@gmail.com>
2021-11-13 20:29:02 +0600

    update

--- index.js
+++ index.js
@@ -21,16 +21,31 @@ async function run() {
     const database = client.db("retros-car");
     const carcollection = database.collection("allcar");
     const orderCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
+    const reviewCollection = database.collection("reviews");
 
-    // get all car
+    // Add Car
     app.post("/addcar", async (req, res) => {
       const car = req.body;
       const result = await carcollection.insertOne(car);
       res.send(car);
     });
-    // Get packages
-    app.get("/cars", async (req, res) => {
+    // Get AllCars
+    app.get("/allcars", async (req, res) => {
       const result = carcollection.find({});
+      const cars = await result.toArray();
+      res.send(cars);
+    });
+    // Get ManageCars
+    app.get("/managecars", async (req, res) => {
+      const result = carcollection.find({});
+      const cars = await result.toArray();
+      res.send(cars);
+    });
+
+    // Get Cars
+    app.get("/cars", async (req, res) => {
+      const result = carcollection.find({}).limit(6);
       const cars = await result.toArray();
       res.send(cars);
     });
@@ -40,10 +55,28 @@ async function run() {
       const orders = req.body;
       const result = await orderCollection.insertOne(orders);
       res.send(result);
-      console.log(orders);
+    });
+    // get reviews
+    app.get("/review", async (req, res) => {
+      const result = reviewCollection.findOne({});
+      const review = await result.toArray();
+      res.send(review);
+    });
+    // Add Reviews
+    app.post("/addreviews", async (req, res) => {
+      const addreviews = req.body;
+      const result = await reviewCollection.insertOne(reviews);
+      res.send(result);
+      console.log(addreviews);
+    });
+    // get single Review
+    app.get("/placereview/:id", async (req, res) => {
+      console.log("getting review", id);
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
       console.log(result);
     });
-
     // get single car
     app.get("/placebooking/:id", async (req, res) => {
       const id = req.params.id;
@@ -72,6 +105,15 @@ async function run() {
       res.json(result);
       console.log(result);
     });
+    // Delet from Manage Car
+    app.delete("/deleteManageOrders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await carcollection.deleteOne(query);
+      res.json(result);
+      console.log(result);
+    });
+
     // Manage All Package Api
     app.get("/allbooking", async (req, res) => {
       const result = orderCollection.find({});
@@ -89,7 +131,45 @@ async function run() {
         },
       });
       res.send(result);
-      console.log(result);
+    });
+    // Get User Api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    app.put("/users", async (req, res) => {
+      console.log("put", user);
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
     });
   } finally {
     //   await client.close();
